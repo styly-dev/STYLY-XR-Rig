69ab13de20eb2c6584144ac0bb5ccc06acb12eb2
Masahiro Yamaguchi <from2001@hotmail.com>
2024-05-09 21:48:26 +0900

    Added manager related script

---
+++ Packages/com.styly.styly-xr-rig/Runtime/Scripts/ARTrackedImageManagerAttacher.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.XR.CoreUtils;
+using UnityEngine.XR.ARFoundation;
+
+namespace Styly.XRRig
+{
+    /// <summary>
+    /// Attach ARTrackedImageManager to XROrigin.
+    /// This script is created for maintenance reasons instead of modifying the original prefab.
+    /// (ARTrackedImageManager can only be attached to XROrigin)
+    /// </summary>
+    public class ARTrackedImageManagerAttacher : MonoBehaviour
+    {
+        void Start()
+        {
+            AttachARTrackedImageManagerToXrOrigin();
+        }
+
+        void AttachARTrackedImageManagerToXrOrigin()
+        {
+            XROrigin xrOrigin = FindObjectOfType<XROrigin>();
+            if (xrOrigin != null)
+            {
+                // Add the ARTrackedImageManager component to the GameObject
+                ARTrackedImageManager arTrackedImageManager = xrOrigin.gameObject.AddComponent<ARTrackedImageManager>();
+                // Disable the ARTrackedImageManager component
+                arTrackedImageManager.enabled = false;
+                Debug.Log("ARTrackedImageManager is attached to XROrigin");
+            }
+        }
+    }
+}
