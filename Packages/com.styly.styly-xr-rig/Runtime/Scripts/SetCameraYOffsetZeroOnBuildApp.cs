a8b6d49aa262229f830351024e6aae50017a9490
Masahiro Yamaguchi <from2001@hotmail.com>
2024-05-12 18:22:34 +0900

    Added editor mode helper

---
+++ Packages/com.styly.styly-xr-rig/Runtime/Scripts/SetCameraYOffsetZeroOnBuildApp.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Styly.XRRig
+{
+    public class SetCameraYOffsetZeroOnBuildApp : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject CameraOffset;
+
+        void Start()
+        {
+            // Skip if running in the editor
+            if (Application.isEditor) { return; }
+
+            // Set the camera offset to zero
+            SetCameraOffsetY_Zero();
+        }
+
+        /// <summary>
+        /// Set the camera offset to zero
+        /// </summary>
+        void SetCameraOffsetY_Zero()
+        {
+            CameraOffset.transform.localPosition = new Vector3(CameraOffset.transform.localPosition.x, 0, CameraOffset.transform.localPosition.z);
+        }
+    }
+}
--- Packages/com.styly.styly-xr-rig/Runtime/Scripts/VolumeCameraConfiguration.cs
+++ Packages/com.styly.styly-xr-rig/Runtime/Scripts/VolumeCameraConfiguration.cs
@@ -7,16 +7,32 @@ namespace Styly.XRRig
 {
     public class VolumeCameraConfiguration : MonoBehaviour
     {
-        public bool UseBoundedModeForVisionOs = false;
-        public VolumeCameraWindowConfiguration BoundedVolumeCamera = null;
-        public VolumeCameraWindowConfiguration UnBoundedVolumeCamera = null;
+        [SerializeField]
+        private bool UseBoundedModeForVisionOs = false;
+        [SerializeField]
+        private VolumeCameraWindowConfiguration BoundedVolumeCamera = null;
+        [SerializeField]
+        private VolumeCameraWindowConfiguration UnBoundedVolumeCamera = null;
+        [SerializeField]
+        private GameObject BoundedGuideFrame = null;
+        [SerializeField]
+        private GameObject CameraOffset = null;
+        [SerializeField]
+        private float CameraHeightInEditor = 1.3f;
 
         // Start is called before the first frame update
         void Start()
         {
+            // Set volume camera configuration
             SetVolumeCameraConfiguration(UseBoundedModeForVisionOs);
+
+            // Disable GuideFrame on build app
+            DisableGuideFrameOnBuildApp();
         }
 
+        /// <summary>
+        /// Set volume camera configuration to VolumeCamera
+        /// </summary>
         void SetVolumeCameraConfiguration(bool useBoundedMode)
         {
             VolumeCamera volumeCamera = this.GetComponentInChildren<VolumeCamera>(true);
@@ -29,5 +45,65 @@ namespace Styly.XRRig
                 volumeCamera.WindowConfiguration = UnBoundedVolumeCamera;
             }
         }
+
+        /// <summary>
+        /// Disable GuideFrame on build app. Only disable in Editor mode.
+        /// </summary>
+        void DisableGuideFrameOnBuildApp()
+        {
+            if (!Application.isEditor)
+            {
+                BoundedGuideFrame.SetActive(false);
+            }
+        }
+
+#if UNITY_EDITOR
+        /// <summary>
+        /// When UseBoundedModeForVisionOs is changed in the editor, update some configurations.
+        /// </summary>
+        void OnValidate()
+        {
+            if (UseBoundedModeForVisionOs)
+            {
+                // Set to Bounded configuration
+                if (BoundedGuideFrame.activeSelf == false)
+                {
+                    // Enable BoundedGuideFrame
+                    BoundedGuideFrame.SetActive(true);
+                    // Changet the size of BoundedGuideFrame to the size of BoundedVolumeCamera
+                    BoundedGuideFrame.transform.localScale = BoundedVolumeCamera.Dimensions;
+                    // Print log
+                    Debug.Log("Display BoundedGuideFrame: On");
+                }
+
+                // Set the camera offset to zero
+                var newCameraOffSet = new Vector3(CameraOffset.transform.localPosition.x, 0, CameraOffset.transform.localPosition.z);
+                if (CameraOffset.transform.localPosition != newCameraOffSet)
+                {
+                    CameraOffset.transform.localPosition = newCameraOffSet;
+                    Debug.Log("Changed CameraOffset: " + CameraOffset.transform.localPosition);
+                }
+            }
+            else
+            {
+                // Set to UnBounded configuration
+                if (BoundedGuideFrame.activeSelf == true)
+                {
+                    // Disable BoundedGuideFrame
+                    BoundedGuideFrame.SetActive(false);
+                    // Print log
+                    Debug.Log("Display BoundedGuideFrame: Off");
+                }
+
+                // Set the camera offset to CameraHeightInEditor
+                var newCameraOffSet = new Vector3(CameraOffset.transform.localPosition.x, CameraHeightInEditor, CameraOffset.transform.localPosition.z);
+                if (CameraOffset.transform.localPosition != newCameraOffSet)
+                {
+                    CameraOffset.transform.localPosition = newCameraOffSet;
+                    Debug.Log("Changed CameraOffset: " + CameraOffset.transform.localPosition);
+                }
+            }
+        }
+# endif
     }
 }
