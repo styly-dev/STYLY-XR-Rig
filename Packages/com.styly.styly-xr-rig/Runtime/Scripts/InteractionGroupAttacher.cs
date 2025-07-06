d505421043e29a832575841432219597b19e47b7
Masahiro Yamaguchi <from2001@hotmail.com>
2025-07-06 20:05:04 +0900

    Added namespace

--- Packages/com.styly.styly-xr-rig/Runtime/Scripts/InteractionGroupAttacher.cs
+++ Packages/com.styly.styly-xr-rig/Runtime/Scripts/InteractionGroupAttacher.cs
@@ -1,36 +1,39 @@
 using UnityEngine;
 
-public class InteractionGroupAttacher : MonoBehaviour
+namespace Styly.XRRig
 {
-        // Set PrimaryInteractionGroup prefab from XRI visionOS sample
-        [SerializeField]
-        private GameObject PrimaryInteractionGroup;
-
-        // Set SecondaryInteractionGroupVariant prefab from XRI visionOS sample
-        [SerializeField]
-        private GameObject SecondaryInteractionGroupVariant;
-
-        // Set _added GameObject inside STYLY XR Rig
-        [SerializeField]
-        private GameObject AttachTo;
-
-        void Awake()
+        public class InteractionGroupAttacher : MonoBehaviour
         {
+                // Set PrimaryInteractionGroup prefab from XRI visionOS sample
+                [SerializeField]
+                private GameObject PrimaryInteractionGroup;
+
+                // Set SecondaryInteractionGroupVariant prefab from XRI visionOS sample
+                [SerializeField]
+                private GameObject SecondaryInteractionGroupVariant;
+
+                // Set _added GameObject inside STYLY XR Rig
+                [SerializeField]
+                private GameObject AttachTo;
+
+                void Awake()
+                {
 #if UNITY_VISIONOS && USE_POLYSPATIAL
                 AttachInteractionGroups();
 #endif
-        }
+                }
 
-        void AttachInteractionGroups()
-        {
-                // Add primary interaction groups to the AttachTo GameObject
-                GameObject primaryGroupInstance = Instantiate(PrimaryInteractionGroup, AttachTo.transform);
-                primaryGroupInstance.name = PrimaryInteractionGroup.name;
+                void AttachInteractionGroups()
+                {
+                        // Add primary interaction groups to the AttachTo GameObject
+                        GameObject primaryGroupInstance = Instantiate(PrimaryInteractionGroup, AttachTo.transform);
+                        primaryGroupInstance.name = PrimaryInteractionGroup.name;
 
-                // Add secondary interaction groups to the AttachTo GameObject
-                GameObject secondaryGroupInstance = Instantiate(SecondaryInteractionGroupVariant, AttachTo.transform);
-                secondaryGroupInstance.name = SecondaryInteractionGroupVariant.name;
+                        // Add secondary interaction groups to the AttachTo GameObject
+                        GameObject secondaryGroupInstance = Instantiate(SecondaryInteractionGroupVariant, AttachTo.transform);
+                        secondaryGroupInstance.name = SecondaryInteractionGroupVariant.name;
 
-                Debug.Log("Interaction groups attached to " + AttachTo.name);
+                        Debug.Log("Interaction groups attached to " + AttachTo.name);
+                }
         }
 }
