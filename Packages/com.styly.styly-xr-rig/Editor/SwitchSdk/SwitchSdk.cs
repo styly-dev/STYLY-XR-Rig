0284f39a766c9737da479f49435b2032b3cc799e
Masahiro Yamaguchi <from2001@hotmail.com>
2025-07-09 22:05:43 +0900

    Add code for each device

---
+++ Packages/com.styly.styly-xr-rig/Editor/SwitchSdk/SwitchSdk.cs
@@ -0,0 +1,105 @@
+using UnityEditor;
+using UnityEngine;
+using static Styly.XRRig.SdkSwitcher.SwitchSdkUtils;
+
+namespace Styly.XRRig.SdkSwitcher
+{
+    public class SwitchSdk
+    {
+        public static void SwitchTo_PicoUnityOpenXrSdk()
+        {
+            // Add SDK Package
+            AddUnityPackage("https://github.com/Pico-Developer/PICO-Unity-OpenXR-SDK.git#release_1.4.0");
+
+            // Enable the OpenXR Loader and set the XR Feature Set
+            EnableXRPlugin(BuildTargetGroup.Android, typeof(UnityEngine.XR.OpenXR.OpenXRLoader));
+            EnableXRFeatureSet(BuildTargetGroup.Android, "com.picoxr.openxr.features");
+
+            // Enable OpenXR Features
+            EnableOpenXrFeatures(BuildTargetGroup.Android, new string[]
+            {
+                "com.unity.openxr.feature.input.handtracking",
+                "com.pico.openxr.feature.passthrough"
+            });
+
+            // Enable Interaction Profiles
+            EnableInteractionProfiles(BuildTargetGroup.Android, new string[]
+            {
+                "com.unity.openxr.feature.input.handinteraction",
+                "com.unity.openxr.feature.input.PICO4touch",
+                "com.unity.openxr.feature.input.PICO4Ultratouch"
+            });
+
+            // ToDo 1
+            // Opne XR Passthroughの右の歯車マークをクリック
+            // ダイアログからis Camera Subsystemをチェック
+
+            // ToDo 2
+            // PICO Supportの右の歯車ボタンを押しPICO Supportダイアログを開く
+            // Feature Settingsから、 Hand Trackingにチェック
+            // Hand Tracking SupportからControllers And Hands を選択
+
+        }
+
+        public static void SwitchTo_ViveOpenXrPlugin()
+        {
+            // Add SDK Package
+            AddUnityPackage("com.htc.upm.vive.openxr@2.5.1");
+
+            // Enable the OpenXR Loader and set the XR Feature Set
+            EnableXRPlugin(BuildTargetGroup.Android, typeof(UnityEngine.XR.OpenXR.OpenXRLoader));
+            EnableXRFeatureSet(BuildTargetGroup.Android, "com.htc.vive.openxr.featureset.vivexr");
+
+            // Enable OpenXR Features
+            EnableOpenXrFeatures(BuildTargetGroup.Android, new string[]
+            {
+                "vive.openxr.feature.compositionlayer",
+                "vive.openxr.feature.hand.tracking",
+                "vive.openxr.feature.passthrough",
+                "com.unity.openxr.feature.vivefocus3"
+            });
+
+            // Enable Interaction Profiles
+            EnableInteractionProfiles(BuildTargetGroup.Android, new string[]
+            {
+                "com.unity.openxr.feature.input.handinteraction",
+                "vive.openxr.feature.focus3controller"
+            });
+
+            // ToDo 1
+            // Graphics APIからVulkanを削除し、OpenGLES3のみにする
+
+            // ToDo 2
+            // Preferences > Graphics > Properties > Advanced PropertiesをAll Visibleにする
+
+            // ToDo 3
+            // HDR設定を変更する
+            // Project Settings > Quality > MobileのRender Pipeline Assetを選択。
+            // QualityのHDRをオフにする
+            // または、QualityのHDRはONのまま、HDR Percisionを64bitに変更する
+
+        }
+
+        public static void SwitchTo_PolySpatialVisionOS()
+        {
+            Debug.LogError("PolySpatial VisionOS SDK is not implemented yet.");
+        }
+
+        public static void SwitchTo_MetaOpenXrSdk()
+        {
+            Debug.LogError("Meta OpenXR SDK is not implemented yet.");
+        }
+
+        public static void RemoveAllSdks()
+        {
+            // Remove all SDK packages from the project
+            RemoveUnityPackage("com.unity.xr.openxr.picoxr");
+            RemoveUnityPackage("com.htc.upm.vive.openxr");
+        }
+
+        public static void TestFunc()
+        {
+            // You can add more test logic here if needed
+        }
+    }
+}
---
+++ Packages/com.styly.styly-xr-rig/Editor/SwitchSdk/SwitchSdkMenu.cs
@@ -0,0 +1,50 @@
+using UnityEditor;
+using UnityEngine;
+
+namespace Styly.XRRig.SdkSwitcher
+{
+    public class SwitchSdkMenu
+    {
+        [MenuItem("File/Switch HMD SDKs/Switch to PICO Unity OpenXR SDK")]
+        static void SwitchTo_PICO()
+        {
+            SwitchSdk.SwitchTo_PicoUnityOpenXrSdk();
+        }
+
+        [MenuItem("File/Switch HMD SDKs/Switch to VIVE OpenXR Plugin")]
+        static void SwitchTo_VIVE()
+        {
+            SwitchSdk.SwitchTo_ViveOpenXrPlugin();
+        }
+
+        [MenuItem("File/Switch HMD SDKs/Switch to Meta OpenXR SDK")]
+        static void SwitchTo_Meta()
+        {
+            SwitchSdk.SwitchTo_MetaOpenXrSdk();
+        }
+
+        [MenuItem("File/Switch HMD SDKs/Switch to PolySpatial (visionOS)")]
+        static void SwitchTo_PolySpatial()
+        {
+            SwitchSdk.SwitchTo_PolySpatialVisionOS();
+        }
+
+        [MenuItem("File/Switch HMD SDKs/Remove all SDK packages")]
+        static void RemoveAllSDKs()
+        {
+            SwitchSdk.RemoveAllSdks();
+        }
+
+        [MenuItem("File/Switch HMD SDKs/[Debug] Debug All Available Info for Android")]
+        static void DebugAllAvailableInfoForAndroid()
+        {
+            SwitchSdkUtils.DebugAllAvailableInfo(BuildTargetGroup.Android);
+        }
+
+        [MenuItem("File/Switch HMD SDKs/Test Function")]
+        static void TestFunc()
+        {
+            SwitchSdk.TestFunc();
+        }
+    }
+}
