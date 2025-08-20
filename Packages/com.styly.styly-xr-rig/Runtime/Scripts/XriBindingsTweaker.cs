0ff8ad0deeda71a5727fec6a9795e9defeb752e4
afjk <afjk01@gmail.com>
2025-08-20 22:07:28 +0900

    Add XriBindingsTweaker for XREAL grab interaction

---
+++ Packages/com.styly.styly-xr-rig/Runtime/Scripts/XriBindingsTweaker.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Styly.XRRig
+{
+    public class XriBindingsTweaker : MonoBehaviour
+    {
+        [SerializeField] private InputActionAsset xriActions;
+
+        void Awake()
+        {
+            var leftSelect = Find("XRI Left Interaction/Select");
+            if (leftSelect != null)
+            {
+                leftSelect.AddBinding("<XREALHandTracking>/indexPressed")
+                    .WithInteraction("Press(pressPoint=0.5)");
+            }
+
+            var rightSelect = Find("XRI Right Interaction/Select");
+            if (rightSelect != null)
+            {
+                rightSelect.AddBinding("<XREALHandTracking>/indexPressed")
+                    .WithInteraction("Press(pressPoint=0.5)");
+            }
+
+            xriActions.Enable();
+        }
+
+        private InputAction Find(params string[] paths)
+        {
+            foreach (var path in paths)
+            {
+                var action = xriActions.FindAction(path, false);
+                if (action != null) return action;
+            }
+
+            return null;
+        }
+    }
+}
