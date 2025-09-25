24a903b077ecc9dee4108b3efc2562700716e698
Masahiro Yamaguchi <from2001@hotmail.com>
2025-09-26 08:29:56 +0900

    Change the location of Setup HMD SDKs menu

--- Packages/com.styly.styly-xr-rig/Editor/SetupSDK/SetupSdkMenu.cs
+++ Packages/com.styly.styly-xr-rig/Editor/SetupSDK/SetupSdkMenu.cs
@@ -8,56 +8,56 @@ namespace Styly.XRRig.SetupSdk
 {
     public class SetupSdkMenu
     {
-        [MenuItem("File/Setup HMD SDKs/Setup PICO Unity OpenXR SDK")]
+        [MenuItem("STYLY/Setup HMD SDKs/Setup PICO Unity OpenXR SDK")]
         static void SwitchTo_PICO()
         {
             EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTargetGroup.Android, BuildTarget.Android);
             SetupSdk_PicoUnityOpenXrSdk.InstallPackage();
         }
 
-        [MenuItem("File/Setup HMD SDKs/Setup VIVE OpenXR Plugin")]
+        [MenuItem("STYLY/Setup HMD SDKs/Setup VIVE OpenXR Plugin")]
         static void SwitchTo_VIVE()
         {
             EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTargetGroup.Android, BuildTarget.Android);
             SetupSdk_ViveOpenXrPlugin.InstallPackage();
         }
 
-        [MenuItem("File/Setup HMD SDKs/Setup Meta OpenXR SDK")]
+        [MenuItem("STYLY/Setup HMD SDKs/Setup Meta OpenXR SDK")]
         static void SwitchTo_Meta()
         {
             EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTargetGroup.Android, BuildTarget.Android);
             SetupSdk_MetaOpenXrSdk.InstallPackage();
         }
 
-        [MenuItem("File/Setup HMD SDKs/Setup PolySpatial (visionOS)")]
+        [MenuItem("STYLY/Setup HMD SDKs/Setup PolySpatial (visionOS)")]
         static void SwitchTo_PolySpatial()
         {
             EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTargetGroup.VisionOS, BuildTarget.VisionOS);
             SetupSdk_PolySpatialVisionOS.InstallPackage();
         }
 
-        [MenuItem("File/Setup HMD SDKs/Setup XREAL SDK")]
+        [MenuItem("STYLY/Setup HMD SDKs/Setup XREAL SDK")]
         static void SwitchTo_Xreal()
         {
             EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTargetGroup.Android, BuildTarget.Android);
             SetupSdk_XrealSdk.InstallPackage();
         }
 
-        [MenuItem("File/Setup HMD SDKs/Setup Android XR")]
+        [MenuItem("STYLY/Setup HMD SDKs/Setup Android XR")]
         static void SwitchTo_AndroidXR()
         {
             EditorUserBuildSettings.SwitchActiveBuildTarget(BuildTargetGroup.Android, BuildTarget.Android);
             SetupSdk_AndroidXR.InstallPackage();
         }
 
-        [MenuItem("File/Setup HMD SDKs/Setup PC VR for Development")]
+        [MenuItem("STYLY/Setup HMD SDKs/Setup PC VR for Development")]
         static void SwitchTo_PcVrForDevelopment()
         {
             SetupSdk_PcVrForDevelopment.InstallPackage();
         }
 
 #if STYLY_EXPERIMENTAL
-        [MenuItem("File/Setup HMD SDKs/[Debug] Debug All Available identifiers for XR")]
+        [MenuItem("STYLY/Setup HMD SDKs/[Debug] Debug All Available identifiers for XR")]
         static void DebugAllAvailableInfoForAndroid()
         {
             SetupSdkUtils.DebugAllAvailableInfo(BuildTargetGroup.Standalone);
@@ -66,7 +66,7 @@ namespace Styly.XRRig.SetupSdk
             SetupSdkUtils.DebugAllAvailableInfo(BuildTargetGroup.VisionOS);
         }
 
-        [MenuItem("File/Setup HMD SDKs/Test Function")]
+        [MenuItem("STYLY/Setup HMD SDKs/Test Function")]
         static void TestFunc()
         {
 
